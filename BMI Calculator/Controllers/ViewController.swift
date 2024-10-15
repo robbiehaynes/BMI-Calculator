de97e4da96a6691283e84014bd6a69b2ee88899e
Robert Haynes <robhaynes0420@gmail.com>
2024-10-15 13:58:44 +0100

    Working UISliders
    The UISliders now display the current value to the user.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = round(sender.value * 100) / 100
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = Int(round(sender.value))
+        weightLabel.text = "\(weight)kg"
+    }
+    
 }
 
